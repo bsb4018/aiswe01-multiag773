91d9785a3f6ec013886cd6b1b80cf7e5662e7b65
Shibendra Bhattacharjee <storebshiv@gmail.com>
2025-03-22 23:42:21 +0530

    Supervisor node added

--- graph.py
+++ graph.py
@@ -19,7 +19,101 @@ llm = ChatOpenAI(
     timeout=None
     )
 
-print(llm.invoke("What is the capital of India?"))
+# print(llm.invoke("What is the capital of India?"))
+
+# The agent state is the input to each node in the graph
+class AgentState(MessagedState):
+    # The next field indicates where to route to next
+    next: str
+
+# Member agent names
+members = []
+
+
+# Node :- Supervisor LLM Node -> \
+# Picks up the next agent to process and \
+# decides when the work is completed
+options = members + ["FINISH"]
+
+system_prompt = (
+    f"""You are a supervisor tasked with managing \
+        a conversation between the following workers: \
+        {members}. Given the following user request, \
+        respond with the worker to act next. """
+)
+
+class SupervisorState(TypedDict):
+    """Worker to route to next. If no wokers needed\
+        route to finish"""
+    next: Literal[*options]
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
